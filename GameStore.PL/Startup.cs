5f61f1f6ab9d2e1f473f7579d6f569a6d3c72d4d
Roman Volyk <Roman_Volyk@epam.com>
2019-09-18 16:23:41 +0300

    updated EF configs

--- GameStore.PL/Startup.cs
+++ GameStore.PL/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using GameStore.DAL.Entity.Context;
+using GameStore.DAL.Entity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,12 +13,17 @@ namespace GameStore.PL
 	{
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+		public void ConfigureServices(IServiceCollection services)
 		{
-            services.AddDbContext<GameStoreContext>(option => {
-                option.UseSqlServer(configuration.GetSection("GameStoreConnection").Value);
-                option.UseLazyLoadingProxies();
-            });
+            //services.AddDbContext<GameStoreContext>(option =>
+            //{
+            //    option.UseSqlServer(configuration.GetSection("GameStoreConnection").Value);
+            //    option.UseLazyLoadingProxies();
+            //});
+            services.AddDbContext<GameStoreContext>(options => {
+            options.UseSqlServer("GameStoreConnection");
+        });
+
         }
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,7 +36,29 @@ namespace GameStore.PL
 
 			app.Run(async (context) =>
 			{
-				await context.Response.WriteAsync("Hello World!");
+                var optionsBuilder = new DbContextOptionsBuilder<GameStoreContext>();
+
+                var options = optionsBuilder
+                    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GameStoreDB;Trusted_Connection=True;")
+                    .UseLazyLoadingProxies()
+                    .Options;
+
+                GameStoreContext db = new GameStoreContext(options);
+
+                Game game = new Game()
+                {
+                    Name = "dfsf"
+                };
+                db.Games.Add(game);
+                db.SaveChanges();
+
+                var list = db.Games;
+                foreach (var l in list)
+                {
+                    await context.Response.WriteAsync(l.Name);
+                }
+
+                await context.Response.WriteAsync("Hello World!");
 			});
 		}
 	}
