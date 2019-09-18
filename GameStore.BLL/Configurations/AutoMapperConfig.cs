32e4df8741645dff92a030be4a14f2a67bef36f2
Roman Volyk <Roman_Volyk@epam.com>
2019-09-18 17:00:00 +0300

    automapper configurated and GameDto added

---
+++ GameStore.BLL/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using GameStore.BLL.DTO;
+using GameStore.DAL.Entity.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameStore.BLL.Configurations
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Game, GameDto>().ReverseMap();
+        }
+
+        public static void Map(IServiceCollection services)
+        {
+            var mappingConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfiles(new Profile[] { new AutoMapperConfig() });
+            });
+            IMapper mapper = mappingConfiguration.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+    }
+}
--- GameStore.PL/Startup.cs
+++ GameStore.PL/Startup.cs
@@ -1,65 +1,46 @@
-﻿using GameStore.DAL.Entity.Context;
-using GameStore.DAL.Entity.Models;
+﻿using GameStore.BLL.Configurations;
+using GameStore.DAL.Entity.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GameStore.PL
 {
-	public class Startup
-	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
-		{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
             //services.AddDbContext<GameStoreContext>(option =>
             //{
             //    option.UseSqlServer(configuration.GetSection("GameStoreConnection").Value);
             //    option.UseLazyLoadingProxies();
             //});
-            services.AddDbContext<GameStoreContext>(options => {
-            options.UseSqlServer("GameStoreConnection");
-        });
+
+            services.AddDbContext<GameStoreContext>(options =>
+            {
+                options.UseSqlServer("GameStoreConnection");
+            });
+
+            AutoMapperConfig.Map(services);
 
         }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-			app.Run(async (context) =>
-			{
-                var optionsBuilder = new DbContextOptionsBuilder<GameStoreContext>();
-
-                var options = optionsBuilder
-                    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GameStoreDB;Trusted_Connection=True;")
-                    .UseLazyLoadingProxies()
-                    .Options;
-
-                GameStoreContext db = new GameStoreContext(options);
-
-                Game game = new Game()
-                {
-                    Name = "dfsf"
-                };
-                db.Games.Add(game);
-                db.SaveChanges();
-
-                var list = db.Games;
-                foreach (var l in list)
-                {
-                    await context.Response.WriteAsync(l.Name);
-                }
-
+            app.Run(async (context) =>
+            {
                 await context.Response.WriteAsync("Hello World!");
-			});
-		}
-	}
+            });
+        }
+    }
 }
